9f2992d29efda503fe6fd5f57e02880a93c1d4e3
GanonKuppa <qtfdl94q@gmail.com>
2020-03-10 01:08:13 +0900

    daを実装

---
+++ src/HardwareAbstractionLayer/hal_da.cpp
@@ -0,0 +1,25 @@
+#include "pd_da.h"
+#include "hal_da.h"
+
+namespcae hal{
+    void initDA(){
+#ifndef SITL
+        periferal_driver::initDA();
+#endif
+    }
+
+    void setDA(float da){
+#ifndef SITL
+        periferal_driver::setDA(da * (DA_RESOLUTION - 1))
+#endif
+    }
+    
+    float getDA(){
+#ifndef SITL
+        return periferal_driver::getDA / (DA_RESOLUTION - 1);
+#else 
+        return 0;
+#endif     
+    }
+
+}
